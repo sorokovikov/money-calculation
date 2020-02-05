8ae8504aa3f44d8f1d0b43a0a47619dd963b5a72
sorokovikov <themilkymouse@gmail.com>
2020-02-05 16:08:37 +0800

    Initial project versions

---
+++ app/forms.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password2 = PasswordField('Повторите пароль', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Регистрация')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Пользователь с таким логином уже существует.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Пользователь с такой почтой уже существует.')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин:', validators=[DataRequired()])
+    password = PasswordField('Пароль:', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
---
+++ app/routes.py
@@ -0,0 +1,119 @@
+from app import app, db, forms
+from app.models import User
+from flask import render_template, request, flash, redirect, request, url_for
+from flask_login import current_user, login_user, login_required, logout_user
+from werkzeug.urls import url_parse
+
+people = []
+food_list = []
+
+
+@app.route("/")
+@app.route("/index")
+@login_required
+def index():
+    return render_template('index.html')
+
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = forms.RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Вы были успешно зарегестрированы.')
+        return redirect(url_for('index'))
+    return render_template('registration.html', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Неверный логин или пароль')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route("/user/<name>")
+def personal_hello(name):
+    return render_template('user.html', name=name.capitalize())
+
+
+@app.route("/new_person", methods=['GET', 'POST'])
+def new_person():
+    form = forms.NameForm()
+    if form.validate_on_submit():
+        flash('Добавлен новый человек ({})'.format(form.name.data))
+        return redirect(url_for('index'))
+    return render_template('new_person.html', form=form)
+
+
+@app.route('/add_person', methods=['POST'])
+def add_person():
+    data = request.json
+    name = data['person']
+    people.append(name)
+    print('Person %s added.' % name)
+    return {'ok': True}
+
+
+@app.route('/add_food', methods=['POST'])
+def add_food():
+    data = request.json
+    food_name = data['food_name'].capitalize()
+    price = int(data['price'])
+    count = int(data['count'])
+    cost = price * count
+    food_list.append({'food_name': food_name,
+                      'price': price,
+                      'count': count,
+                      'cost': cost})
+    print(food_list)
+    print('Food %s added.' % food_name)
+    return {'ok': True}
+
+
+@app.route('/change_food', methods=['POST'])
+def change_food():
+    data = request.json
+    number = data['number']
+    new_food_name = data['food_name']
+    food_list[number]['food_name'] = new_food_name
+    return {'ok': True}
+
+
+@app.route('/food_list')
+def get_food_list():
+    total_cost = 0
+    for food in food_list:
+        total_cost += food['cost']
+    return render_template('food_list.html', food_list=food_list, total_cost=total_cost)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
