27f53035749e80c8618e4c6f65c099d8db064879
sorokovikov <themilkymouse@gmail.com>
2020-02-18 18:44:46 +0800

    Fix validating username in EditProfileForm, Added logging

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from logging.handlers import RotatingFileHandler
+import logging
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,3 +15,14 @@ login.login_view = 'login'
 migrate = Migrate(app, db)
 
 from app import routes, models, errors
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/moneyCalculation.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('MoneyCalculation app')
--- app/routes.py
+++ app/routes.py
@@ -73,7 +73,7 @@ def user(username):
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = forms.EditProfileForm()
+    form = forms.EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.status = form.status.data
